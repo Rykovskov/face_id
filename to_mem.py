5a321a98ccf37734eacd2f23400d122ab68f7cb5
Rykovskov <51448342+Rykovskov@users.noreply.github.com>
2020-02-08 22:35:59 +0300

    Edit to_mem

--- to_mem.py
+++ to_mem.py
@@ -6,8 +6,60 @@ import time
 import sys
 import traceback
 
+size_pic_with = 1920
+size_pic_high = 1080
+delta_size_x = 40
+delta_size_y = 60
+
 def rescale_frame(frame, percent=75):
     width = int(frame.shape[1] * percent/ 100)
     height = int(frame.shape[0] * percent/ 100)
     dim = (width, height)
     return cv2.resize(frame, dim, interpolation =cv2.INTER_AREA)
+def normal_rect(x1,y1,x2,y2):
+    x1 = x1 - 40
+    x2 = x2 + 40
+    y1 = y1 - 60
+    y2 = y2 + 60
+    if x1 < 0:
+        x1 = 0
+    if y1 < 0:
+        y1 = 0
+    if x2 > size_pic_with:
+        x2 = size_pic_with
+    if y2 > size_pic_high:
+        y2 = size_pic_high
+    return x1, y1, x2, y2
+
+cnn_face_detector = dlib.cnn_face_detection_model_v1("mmod_human_face_detector.dat")
+win = dlib.image_window()
+
+vcap = cv2.VideoCapture("rtsp://admin@192.168.21.168:554/user=admin&password=&channel=1&stream=0")
+while True:
+    try:
+        ret, frame = vcap.read()
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        resc_frame = rescale_frame(rgb_frame, percent=60)
+        t1 = time.time()
+        dets = cnn_face_detector(resc_frame, 1)
+        if len(dets) > 0:
+           for i, d in enumerate(dets):
+               print("Detection {}: Left: {} Top: {} Right: {} Bottom: {} Confidence: {}".format(
+                       i, d.rect.left(), d.rect.top(), d.rect.right(), d.rect.bottom(), d.confidence))
+               x_left, y_top, x_right, y_bottom = normal_rect(d.rect.left(), d.rect.top(), d.rect.right(), d.rect.bottom())
+               face_img =frame[x_left:y_top, x_right:y_bottom]
+               filename = datetime.datetime.now().strftime("%d%m%Y__%H_%M_%S")+".jpg"
+               cv2.imwrite(filename, face_img)
+           t2 = time.time()
+           print("Time:", (t2-t1))
+           #rects = dlib.rectangles()
+           #rects.extend([d.rect for d in dets])
+           #win.clear_overlay()
+           win.set_image(resc_frame)
+           #win.add_overlay(rects)
+           t3=time.time()
+           print("Full Time: ",(t3-t1))
+        if cv2.waitKey(5) == ord('a'):
+           break
+    except:
+        print('Ошибка:\n', traceback.format_exc())
