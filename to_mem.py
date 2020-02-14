1e4fce12176bfbc389432a5735ab223b6db3a310
Rykovskov <51448342+Rykovskov@users.noreply.github.com>
2020-02-14 23:46:41 +0300

    Edit to_mem

--- to_mem.py
+++ to_mem.py
@@ -1,18 +1,67 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+import os
 import cv2
-import dlib
+#import dlib
 import numpy as np
 import time
 import sys
 import traceback
 import datetime
+import mrcnn.config
+import mrcnn.utils
+from pathlib import Path
+from mrcnn.model import MaskRCNN
 
 size_pic_with = 1920
 size_pic_high = 1080
 delta_size_x = 40
 delta_size_y = 90
 max_broken_frame = 100
+
+ROOT_DIR = Path(".")
+CAR_DIR = os.path.join(ROOT_DIR, "car_img")
+HUMAN_DIR = os.path.join(ROOT_DIR, "human_img")
+PET_DIR = os.path.join(ROOT_DIR, "pet_img")
+
+# Directory to save logs and trained model
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+# Local path to trained weights file
+COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+# Download COCO trained weights from Releases if needed
+if not os.path.exists(COCO_MODEL_PATH):
+    mrcnn.utils.download_trained_weights(COCO_MODEL_PATH)
+
+class MaskRCNNConfig(mrcnn.config.Config):
+    NAME = "coco_pretrained_model_config"
+    IMAGES_PER_GPU = 1
+    GPU_COUNT = 1
+    NUM_CLASSES = 1 + 80  # COCO dataset has 80 classes + one background class
+    DETECTION_MIN_CONFIDENCE = 0.6
+
+def get_car_boxes(boxes, class_ids):
+    car_boxes = []
+    for i, box in enumerate(boxes):
+        # If the detected object isn't a car / truck, skip it
+        if class_ids[i] in [2, 3, 8, 6, 4,]:
+            car_boxes.append(box)
+
+    return np.array(car_boxes)
+def get_human_boxes(boxes, class_ids):
+    human_boxes = []
+    for i, box in enumerate(boxes):
+        if class_ids[i] in [1, ]:
+            human_boxes.append(box)
+
+    return np.array(human_boxes)
+def get_pet_boxes(boxes, class_ids):
+    pet_boxes = []
+    for i, box in enumerate(boxes):
+        if class_ids[i] in [16, 17, 18]:
+            pet_boxes.append(box)
+
+    return np.array(pet_boxes)
+
 
 def rescale_frame(frame, percent=75):
     width = int(frame.shape[1] * percent/ 100)
@@ -34,7 +83,13 @@ def normal_rect(x1,y1,x2,y2):
         y2 = size_pic_high
     return x1, y1, x2, y2
 
-cnn_face_detector = dlib.cnn_face_detection_model_v1("mmod_human_face_detector.dat")
+#Load Dlib
+#cnn_face_detector = dlib.cnn_face_detection_model_v1("mmod_human_face_detector.dat")
+
+# Create a Mask-RCNN model in inference mode
+model = MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=MaskRCNNConfig())
+# Load pre-trained model
+model.load_weights(COCO_MODEL_PATH, by_name=True)
 
 try:
     vcap = cv2.VideoCapture("rtsp://admin@192.168.21.168:554/user=admin&password=&channel=1&stream=0")
@@ -44,44 +99,72 @@ except:
 counter_broken_frame = 0
 while True:
     try:
-        try:
-            ret, frame = vcap.read()
-            if frame is None:
-                counter_broken_frame = counter_broken_frame + 1
-                print(counter_broken_frame)
-        except:
+        ret, frame = vcap.read()
+        if not ret:
             counter_broken_frame = counter_broken_frame + 1
             print(counter_broken_frame)
+            break
         if counter_broken_frame > max_broken_frame:
             print("Unable reading source video !!!")
             sys.exit()
-        #rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        resc_frame = rescale_frame(frame, percent=80)
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        #resc_frame = rescale_frame(frame, percent=80)
         t1 = time.time()
-        dets = cnn_face_detector(resc_frame, 1)
-        if len(dets) > 0:
-           for i, d in enumerate(dets):
-               print("Detection {}: Left: {} Top: {} Right: {} Bottom: {} Confidence: {}".format(
-                       i, d.rect.left(), d.rect.top(), d.rect.right(), d.rect.bottom(), d.confidence))
-               x_left, y_top, x_right, y_bottom = normal_rect(d.rect.left(), d.rect.top(), d.rect.right(), d.rect.bottom())
-               face_img =resc_frame[y_top:y_bottom, x_left:x_right]
-               filename = datetime.datetime.now().strftime("%d%m%Y__%H_%M_%S")+".jpg"
-               cv2.imwrite(filename, face_img)
-               # filename = datetime.datetime.now().strftime("%d%m%Y__%H_%M_%S") + "_resc.jpg"
-               # cv2.imwrite(filename, resc_frame)
-               # filename = datetime.datetime.now().strftime("%d%m%Y__%H_%M_%S") + "_rgb.jpg"
-               # cv2.imwrite(filename, rgb_frame)
-               filename = datetime.datetime.now().strftime("%d%m%Y__%H_%M_%S") + "_org.jpg"
-               cv2.imwrite(filename, frame)
-           t2 = time.time()
-           print("Time:", (t2-t1))
-           #rects = dlib.rectangles()
-           #rects.extend([d.rect for d in dets])
-           #win.clear_overlay()
-           #win.set_image(resc_frame)
-           #win.add_overlay(rects)
-           t3=time.time()
-           print("Full Time: ", (t3-t1))
+        # Run the image through the Mask R-CNN model to get results.
+        results = model.detect([rgb_frame], verbose=0)
+        t2 = time.time()
+        print("Time detect all object:", (t2 - t1))
+        r = results[0]
+        car_boxes = get_car_boxes(r['rois'], r['class_ids'])
+        human_boxes = get_human_boxes(r['rois'], r['class_ids'])
+        pet_boxes = get_pet_boxes(r['rois'], r['class_ids'])
+        # Draw each box on the frame
+        for box in car_boxes:
+            print("Car: ", box)
+            y1, x1, y2, x2 = box
+            #Save image to disk
+            img = rgb_frame[y1:y2, x1:x2]
+            filename = os.path.join(CAR_DIR, datetime.datetime.now().strftime("%d%m%Y__%H_%M_%S") + ".jpg")
+            cv2.imwrite(filename, img)
+        for box in human_boxes:
+            print("Human: ", box)
+            y1, x1, y2, x2 = box
+            # Save image to disk
+            img = rgb_frame[y1:y2, x1:x2]
+            filename = os.path.join(HUMAN_DIR, datetime.datetime.now().strftime("%d%m%Y__%H_%M_%S") + ".jpg")
+            cv2.imwrite(filename, img)
+        for box in pet_boxes:
+            print("Pet: ", box)
+            y1, x1, y2, x2 = box
+            # Save image to disk
+            img = rgb_frame[y1:y2, x1:x2]
+            filename = os.path.join(PET_DIR, datetime.datetime.now().strftime("%d%m%Y__%H_%M_%S") + ".jpg")
+            cv2.imwrite(filename, img)
+
+        # dets = cnn_face_detector(resc_frame, 1)
+        # if len(dets) > 0:
+        #    for i, d in enumerate(dets):
+        #        print("Detection {}: Left: {} Top: {} Right: {} Bottom: {} Confidence: {}".format(
+        #                i, d.rect.left(), d.rect.top(), d.rect.right(), d.rect.bottom(), d.confidence))
+        #        x_left, y_top, x_right, y_bottom = normal_rect(d.rect.left(), d.rect.top(), d.rect.right(), d.rect.bottom())
+        #        face_img =resc_frame[y_top:y_bottom, x_left:x_right]
+        #        filename = datetime.datetime.now().strftime("%d%m%Y__%H_%M_%S")+".jpg"
+        #        cv2.imwrite(filename, face_img)
+        #        # filename = datetime.datetime.now().strftime("%d%m%Y__%H_%M_%S") + "_resc.jpg"
+        #        # cv2.imwrite(filename, resc_frame)
+        #        # filename = datetime.datetime.now().strftime("%d%m%Y__%H_%M_%S") + "_rgb.jpg"
+        #        # cv2.imwrite(filename, rgb_frame)
+        #        filename = datetime.datetime.now().strftime("%d%m%Y__%H_%M_%S") + "_org.jpg"
+        #        cv2.imwrite(filename, frame)
+        #    t2 = time.time()
+        #    print("Time:", (t2-t1))
+        #    #rects = dlib.rectangles()
+        #    #rects.extend([d.rect for d in dets])
+        #    #win.clear_overlay()
+        #    #win.set_image(resc_frame)
+        #    #win.add_overlay(rects)
+        t3 = time.time()
+        print("Full Time: ", (t3-t1))
     except:
         print('Ошибка:\n', traceback.format_exc())
         sys.exit()
