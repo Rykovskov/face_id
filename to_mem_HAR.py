793593cbfe86b69c53fd03c92436fff36184a756
Rykovskov <51448342+Rykovskov@users.noreply.github.com>
2020-04-16 17:31:26 +0300

    Add har cascade

--- to_mem_HAR.py
+++ to_mem_HAR.py
@@ -100,8 +100,10 @@ while True:
     if fm is None:
         fm = 'None'
     font = cv2.FONT_HERSHEY_DUPLEX
+    ss = fm + ' gg'
+    print(ss)
     #print("fn - ", fm)
-    cv2.putText(frame, fm, (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 2, 255)
+    cv2.putText(frame, ss, (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 2, 255)
     #cv2.putText(frame, fm, (200, 200), font, 0.5, (0, 0, 255), 1)
     filename1 = os.path.join(CAR_DIR, 'full_' + datetime.datetime.now().strftime("%d%m%Y__%H_%M_%S") + ".jpg")
     cv2.imwrite(filename1, frame)
