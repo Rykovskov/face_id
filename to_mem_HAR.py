116b2bbb00f8aa996f166f002ce92e0aae16fc44
Rykovskov <51448342+Rykovskov@users.noreply.github.com>
2020-04-22 00:08:05 +0300

    Add har cascade

--- to_mem_HAR.py
+++ to_mem_HAR.py
@@ -10,6 +10,7 @@ import datetime
 import compare_pic
 from pathlib import Path
 import imutils
+import psycopg2
 
 size_pic_with = 1920
 size_pic_high = 1080
@@ -18,10 +19,7 @@ delta_size_y = 90
 max_broken_frame = 1000
 min_dist_pic = 0.6
 
-ROOT_DIR = Path("/mnt/v1/TF")
-CAR_DIR = os.path.join(ROOT_DIR, "car_img")
-HUMAN_DIR = os.path.join(ROOT_DIR, "human_img")
-PET_DIR = os.path.join(ROOT_DIR, "pet_img")
+ROOT_DIR = Path("/home/max/base")
 
 cascade_car = 'cars.xml'
 car_cascade = cv2.CascadeClassifier(cascade_car)
@@ -30,14 +28,9 @@ human_cascade = cv2.CascadeClassifier(cascade_human)
 cascade_dog = 'haarcascade_frontalface_alt.xml'
 dog_cascade = cv2.CascadeClassifier(cascade_dog)
 
-def detect(img, cascade):
-    rects = cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=4, minSize=(430, 430),
-                                     flags=cv2.CASCADE_SCALE_IMAGE)
-    if len(rects) == 0:
-        return []
-    rects[:,2:] += rects[:,:2]
-    return rects
-
+conn = psycopg2.connect(dbname='trafik', user='max', password='tv60hu02', host='192.168.21.48')
+sql_insert_actions = """insert into actions (dt_actions, patch_to_pic, name_pic) values (%s, %s, %s)  RETURNING id_actions;"""
+sql_insert_rects = """insert into rectangles (id_actions, x1, x2, y1, y2) values (%s, %s, %s, %s, %s);"""
 
 def draw_rects(img, rects, color):
     for x1, y1, x2, y2 in rects:
@@ -49,6 +42,7 @@ def rescale_frame(frame, percent=75):
     height = int(frame.shape[0] * percent/ 100)
     dim = (width, height)
     return cv2.resize(frame, dim, interpolation =cv2.INTER_AREA)
+
 
 def normal_rect(x1, y1, x2, y2):
     x1 = x1 - delta_size_x
@@ -97,10 +91,10 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.equalizeHist(gray)
     fm = variance_of_laplacian(gray)
-    #if fm > 19000.00:
-    #    continue
+    if fm > 19000.00:
+        # Skip unfocused images
+        continue
     font = cv2.FONT_HERSHEY_DUPLEX
-    ss = str(fm) + ' gg'
     #Car
     rects = car_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4, minSize=(330, 330), flags=cv2.CASCADE_SCALE_IMAGE)
     if len(rects) == 0:
@@ -108,18 +102,32 @@ while True:
     else:
         rects[:, 2:] += rects[:, :2]
     vis = frame.copy()
+    now = datetime.datetime.now()
     for x1, y1, x2, y2 in rects:
-        filename = os.path.join(CAR_DIR, datetime.datetime.now().strftime("%d%m%Y__%H_%M_%S") + ".jpg")
+        # Make directory if not exist
+        year_str = datetime.datetime.now().strftime("%Y")
+        month_str = datetime.datetime.now().strftime("%m")
+        day_str = datetime.datetime.now().strftime("%d")
+        fullPath = os.path.join(ROOT_DIR, year_str, month_str, day_str)
+        if not os.path.exists(fullPath):
+            os.mkdir(fullPath)
+        filename = datetime.datetime.now().strftime("%H_%M_%S") + ".jpg"
+        fullPath = os.path.join(fullPath, filename)
         print("Car ! ", x1, y1, x2, y2)
         (x1, y1, x2, y2) = normal_rect(x1, y1, x2, y2)
-        img_car = frame[y1:y2, x1:x2]
+        #img_car = frame[y1:y2, x1:x2]
         #roi = gray[y1:y2, x1:x2]
         #vis_roi = vis[y1:y2, x1:x2]
-        draw_rects(vis, rects, (0, 255, 0))
-        cv2.imwrite(filename, img_car)
-        cv2.putText(vis, ss, (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 2, 255)
-        filename1 = os.path.join(CAR_DIR, 'full_' + datetime.datetime.now().strftime("%d%m%Y__%H_%M_%S") + ".jpg")
-        cv2.imwrite(filename1, vis)
+        #draw_rects(vis, rects, (0, 255, 0))
+        #cv2.imwrite(filename, img_car)
+        cv2.putText(vis, str(fm), (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 2, 255)
+        cv2.imwrite(fullPath, vis)
+        cur = conn.cursor()
+        cur.execute(sql_insert_actions, (now, fullPath, filename, ))
+        id_of_new_row = cur.fetchone()[0]
+        cur.execute(sql_insert_rects, (id_of_new_row, x1, x2, y1, y2,))
+        conn.commit()
+        cur.close()
     #Human
     rects = human_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4, minSize=(130, 230),
                                          flags=cv2.CASCADE_SCALE_IMAGE)
@@ -129,17 +137,52 @@ while True:
         rects[:, 2:] += rects[:, :2]
     vis = frame.copy()
     for x1, y1, x2, y2 in rects:
-        filename = os.path.join(HUMAN_DIR, datetime.datetime.now().strftime("%d%m%Y__%H_%M_%S") + ".jpg")
+        # Make directory if not exist
+        year_str = datetime.datetime.now().strftime("%Y")
+        month_str = datetime.datetime.now().strftime("%m")
+        day_str = datetime.datetime.now().strftime("%d")
+        fullPath = os.path.join(ROOT_DIR, year_str, month_str, day_str)
+        if not os.path.exists(fullPath):
+            os.mkdir(fullPath)
+        filename = datetime.datetime.now().strftime("%H_%M_%S") + ".jpg"
+        fullPath = os.path.join(fullPath, filename)
         print("Human ! ", x1, y1, x2, y2)
         (x1, y1, x2, y2) = normal_rect(x1, y1, x2, y2)
-        img_human = frame[y1:y2, x1:x2]
-        #roi = gray[y1:y2, x1:x2]
-        # vis_roi = vis[y1:y2, x1:x2]
-        draw_rects(vis, rects, (0, 255, 0))
-        cv2.imwrite(filename, img_human)
-        cv2.putText(frame, ss, (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 2, 255)
-        filename1 = os.path.join(HUMAN_DIR, 'full_' + datetime.datetime.now().strftime("%d%m%Y__%H_%M_%S") + ".jpg")
-        cv2.imwrite(filename1, vis)
-
+        cv2.putText(vis, str(fm), (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 2, 255)
+        cv2.imwrite(fullPath, vis)
+        cur = conn.cursor()
+        cur.execute(sql_insert_actions, (now, fullPath, filename,))
+        id_of_new_row = cur.fetchone()[0]
+        cur.execute(sql_insert_rects, (id_of_new_row, x1, x2, y1, y2,))
+        conn.commit()
+        cur.close()
+    #Pets
+    rects = cascade_dog.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4, minSize=(130, 230),
+                                         flags=cv2.CASCADE_SCALE_IMAGE)
+    if len(rects) == 0:
+        rects = []
+    else:
+        rects[:, 2:] += rects[:, :2]
+    vis = frame.copy()
+    for x1, y1, x2, y2 in rects:
+        # Make directory if not exist
+        year_str = datetime.datetime.now().strftime("%Y")
+        month_str = datetime.datetime.now().strftime("%m")
+        day_str = datetime.datetime.now().strftime("%d")
+        fullPath = os.path.join(ROOT_DIR, year_str, month_str, day_str)
+        if not os.path.exists(fullPath):
+            os.mkdir(fullPath)
+        filename = datetime.datetime.now().strftime("%H_%M_%S") + ".jpg"
+        fullPath = os.path.join(fullPath, filename)
+        print("Human ! ", x1, y1, x2, y2)
+        (x1, y1, x2, y2) = normal_rect(x1, y1, x2, y2)
+        cv2.putText(vis, str(fm), (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 2, 255)
+        cv2.imwrite(fullPath, vis)
+        cur = conn.cursor()
+        cur.execute(sql_insert_actions, (now, fullPath, filename,))
+        id_of_new_row = cur.fetchone()[0]
+        cur.execute(sql_insert_rects, (id_of_new_row, x1, x2, y1, y2,))
+        conn.commit()
+        cur.close()
 
 
