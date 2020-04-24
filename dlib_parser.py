5f73b7faace4c368052d6f1fb2b01808d175157f
Rykovskov <51448342+Rykovskov@users.noreply.github.com>
2020-04-24 22:39:46 +0300

    Add Dlib parser

--- dlib_parser.py
+++ dlib_parser.py
@@ -57,7 +57,14 @@ sp = dlib.shape_predictor(predictor_path)
 detector = dlib.get_frontal_face_detector()
 
 conn = psycopg2.connect(dbname='trafik', user='max', password='tv60hu02', host='localhost')
-sql_select_humans = """select id_humans, id_actions, patch_to_pic from humans where dlib_parser=false;"""
+sql_select_humans = """select id_humans, actions.id_actions, dt_actions, humans.patch_to_pic 
+                        from humans 
+                        left join actions on actions.id_actions=humans.id_actions 
+                        where dlib_parser=false"""
+
+sql_insert_face = """insert into faces (id_actions, id_humans, patch_to_pic, keypoints) values (%s, %s, %s, %s);"""
+
+sql_update_humans_dlib = """update humans set dlib_parser=true where id_humans=%s"""
 
 cur = conn.cursor()
 cur.execute(sql_select_humans)
@@ -66,7 +73,63 @@ if len(records) == 0:
     cur.close()
     conn.close()
     exit()
-for (id_humans, id_actions, patch_to_pic) in records:
+for (id_humans, id_actions, dt, patch_to_pic) in records:
     if os.path.isfile(patch_to_pic):
         frame = cv2.imread(patch_to_pic)
         find_object = 0
+        dets = detector(frame, 1)
+        s_images = []
+        for k, d in enumerate(dets):
+            x_face1 = d.left() - 40
+            x_face2 = d.right() + 40
+            y_face1 = d.top() - 60
+            y_face2 = d.bottom() + 60
+            if x_face1 < 0:
+                x_face1 = 0
+            if y_face1 < 0:
+                y_face1 = 0
+            if x_face2 > 1980:
+                x_face2 = 1980
+            if y_face2 > 1024:
+                y_face2 = 1024
+            s_images.append(frame[y_face1:y_face2, x_face1:x_face2])
+        num_faces = len(dets)
+        if num_faces > 0:
+            print("Find face! ", num_faces)
+            cur.execute(sql_update_humans_dlib, (id_humans,))
+            conn.commit()
+            faces = dlib.full_object_detections()
+            for detection in dets:
+                faces.append(sp(frame, detection))
+            # Get the aligned face images
+            # Optionally:
+            images = dlib.get_face_chips(frame, faces, size=320, padding=0.25)
+            # images = dlib.get_face_chips(rgb_image, faces, size=640, padding=0.25)
+            i = 0
+            year_str = dt.strftime("%Y")
+            month_str = dt.strftime("%m")
+            day_str = dt.strftime("%d")
+            for image in images:
+                try:
+                    # Make directory if not exist
+                    fullPath = os.path.join(FACES_DIR, year_str, month_str, day_str)
+                    if not os.path.exists(fullPath):
+                        os.makedirs(fullPath)
+                    filename = dt.strftime("%H_%M_%S") + ".jpg"
+                    fullPath = os.path.join(fullPath, filename)
+                    img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+                    dets1 = detector(image, 1)
+                    for k1, d1 in enumerate(dets1):
+                        shape1 = sp(image, d1)
+                    face_descriptor1 = facerec.compute_face_descriptor(image, shape1)
+                    out_dump1 = pickle.dumps(face_descriptor1, 1)
+                    cur.execute(sql_insert_face, (id_actions, id_humans, fullPath, psycopg2.Binary(out_dump1)))
+                    cv2.imwrite(fullPath, s_images[i])
+                    conn.commit()
+                    print("Save image complete!!")
+                except psycopg2.Error as e:
+                    print(e.pgerror)
+                    print(e.diag.message_detail)
+                    print('Ошибка:\n', traceback.format_exc())
+                i = i + 1
+
