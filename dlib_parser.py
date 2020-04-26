1e4b1cb0289e208fbf73182602c7e38329104552
Rykovskov <51448342+Rykovskov@users.noreply.github.com>
2020-04-26 22:03:08 +0300

    Add Dlib parser

--- dlib_parser.py
+++ dlib_parser.py
@@ -23,6 +23,16 @@ def addapt_numpy_int64(numpy_int64):
     return AsIs(numpy_int64)
 
 
+def pickle_keypoints(keypoints, descriptors):
+    i = 0
+    temp_array = []
+    for point in keypoints:
+        temp = (point.pt, point.size, point.angle, point.response, point.octave,
+        point.class_id, descriptors[i])
+        ++i
+        temp_array.append(temp)
+    return temp_array
+
 ROOT_DIR = Path("/home/max/base")
 APP_DIR = Path("/home/max/face_id")
 FACES_DIR = os.path.join(ROOT_DIR, "faces")
@@ -44,6 +54,7 @@ sql_select_humans = """select id_humans, actions.id_actions, dt_actions, humans.
                         where dlib_parser=false"""
 
 sql_insert_face = """insert into faces (id_actions, id_humans, patch_to_pic) values (%s, %s, %s);"""
+sql_insert_face_des = """insert into faces (id_actions, id_humans, patch_to_pic, keypoints) values (%s, %s, %s, %s);"""
 
 sql_update_humans_dlib = """update humans set dlib_parser=true where id_humans=%s"""
 
@@ -57,23 +68,44 @@ if len(records) == 0:
 for (id_humans, id_actions, dt, patch_to_pic) in records:
     if os.path.isfile(patch_to_pic):
         frame = cv2.imread(patch_to_pic)
+        year_str = dt.strftime("%Y")
+        month_str = dt.strftime("%m")
+        day_str = dt.strftime("%d")
+        fullPath = os.path.join(FACES_DIR, year_str, month_str, day_str)
         find_object = 0
-        dets = cnn_face_detector(frame, 1)
+        #lendmark detector
+        descriptors = []
+        s_images=[]
+        dets = detector(frame, 1)
+        num_faces = len(dets)
+        if num_faces > 0:
+            if not os.path.exists(fullPath):
+                os.makedirs(fullPath)
+            for k, d in enumerate(dets):
+                # Get the landmarks/parts for the face in box d.
+                shape = sp(img, d)
+                # Compute the 128D vector that describes the face in img identified by
+                # shape.
+                face_descriptor = facerec.compute_face_descriptor(img, shape)
+                descriptors.append(face_descriptor)
+                s_images.append(frame[d.top():d.bottom(), d.left():d.right()])
+            i = 0
+            for img in s_images:
+                filename = dt.strftime("%H_%M_%S_%f_") + str(i) + ".jpg"
+                fullPath = os.path.join(fullPath, filename)
+                cur.execute(sql_update_humans_dlib, (id_humans,))
+                conn.commit()
+                out_dump = pickle.dumps(descriptors[i], 1)
+                cur.execute(sql_insert_face_des, (id_actions, id_humans, fullPath, (psycopg2.Binary(out_dump))))
+                cv2.imwrite(fullPath, img)
+                conn.commit()
+                i = i + 1
+            continue
+        #CNN Detector
+        dets_cnn = cnn_face_detector(frame, 1)
         s_images = []
-        for k, d in enumerate(dets):
-            x_face1 = d.rect.left() - 40
-            x_face2 = d.rect.right() + 40
-            y_face1 = d.rect.top() - 60
-            y_face2 = d.rect.bottom() + 60
-            if x_face1 < 0:
-                x_face1 = 0
-            if y_face1 < 0:
-                y_face1 = 0
-            if x_face2 > 1980:
-                x_face2 = 1980
-            if y_face2 > 1024:
-                y_face2 = 1024
-            s_images.append(frame[y_face1:y_face2, x_face1:x_face2])
+        for k, d in enumerate(dets_cnn):
+            s_images.append(frame[d.rect.top():d.rect.bottom(), d.rect.left():d.rect.right()])
         num_faces = len(dets)
         if num_faces > 0:
             year_str = dt.strftime("%Y")
